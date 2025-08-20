0023995c6326a534a856e5e3982095cb382093de
CruzM-224 <77972110+CruzM-224@users.noreply.github.com>
2025-08-20 10:31:37 -0600

    Agregar vista Sobre Nosotros

--- TechFood-Solutions/Controllers/HomeController.cs
+++ TechFood-Solutions/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace TechFood_Solutions.Controllers
             return View();
         }
 
+        public ActionResult SobreNosotros()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
