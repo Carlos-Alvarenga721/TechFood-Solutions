be11eb9974d04a34017473fbdc5a16f67fda3d60
Carlos Alvarenga <c.alvarenga.c7@gmail.com>
2025-08-20 08:21:03 -0600

    subiendo footer y header

---
+++ TechFood-Solutions/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechFood_Solutions.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
